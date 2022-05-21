c5b902d1d5a07dda505d532bc010e4c5d85b7482
Riley-I <riley.inniss@gmail.com>
2022-05-21 23:06:14 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Random Response Generator - OpenAi Challenge
+
+## Description
+
+GPT-3 is a powerful AI model created by OpenAI. It can process plain text prompts and produce outputs that are hard to distinguish from human writing. Check out some examples of what it can do! GPT-3 can be accessed through a public API that includes a generous amount of free credits. 
+
+## Challenge
+
+You will write an app that sends plain text prompts to the OpenAI API and displays the results in a list.
+
+    -We'd like your app to have a simple-to-use interface that includes the following:
+    -A form for entering text prompts
+    -Submitting the form sends the prompt to the OpenAI API
+    -Results are displayed in a list, sorted from newest to oldest. Each result should include the original prompt and a response from the API.
+
+
+Bootstrap was used for the front end framework and styled with custom CSS.
---
+++ script.js
@@ -0,0 +1,152 @@
+
+//OPEN API CODING CHALLENGE
+//https://beta.openai.com
+//RILEY INNISS | rileyidev.ca | info@rileyidev.ca
+
+
+
+//////////////////SENDING REQUESTS////////////////
+
+//data object
+const data = {
+    prompt: "",
+    temperature: 0.9, //higher = more risks
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+   };
+
+
+//Display prompt
+function getPrompt(data){
+
+  let promptValue = document.getElementById("prompt").value;
+  data.prompt = promptValue; //push prompt value into data object
+  console.log(data);
+
+  const promptDiv = document.getElementById("writePrompt");
+  const li = document.createElement("li");
+  li.innerHTML = promptValue;
+  promptDiv.appendChild(li);
+}
+
+
+//SEND REQUEST
+function sendRequest(){
+    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        //   Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+          Authorization: `Bearer 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`,
+        },
+        body: JSON.stringify(data),
+       })
+       .then(response => response.json())
+        .then(data => {
+    
+        let responseArray = data.choices;
+    
+        for (let i = 0; i < responseArray.length; i++) {
+            console.log(responseArray[i].text);
+
+            let response = responseArray[i].text;
+            console.log(response);
+
+            const responseDiv = document.getElementById("writeResponse");
+            const liResponse = document.createElement("li");
+            liResponse.classList.add("response");
+            liResponse.innerHTML = response;
+            responseDiv.appendChild(liResponse);
+          } 
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
+}
+
+
+////////////////SUGGESTIONS////////////////
+
+var myElementOne = document.getElementById("suggestionOne");
+var myElementTwo = document.getElementById("suggestionTwo");
+var myElementThree = document.getElementById("suggestionThree");
+
+function getSuggestionsFirst(myElementOne){
+  console.log(myElementOne);
+  let btn = document.createElement("button");
+  btn.innerHTML = "Select";
+
+      btn.onclick  = function() {
+          let promptValue = document.getElementById("suggestionOne").innerText;
+          // console.log(promptValue);
+          data.prompt = promptValue;
+          console.log(data);
+          const promptDiv = document.getElementById("writePrompt");
+          const li = document.createElement("li");
+          li.innerHTML = promptValue;
+          promptDiv.appendChild(li);
+
+          sendRequest()
+        }
+  //console.log(data);
+  myElementOne.after(btn);
+}
+
+function getSuggestionsSecond(myElementTwo){
+  console.log(myElementTwo);
+  let btn = document.createElement("button");
+  btn.innerHTML = "Select";
+
+      btn.onclick  = function() {
+          let promptValue = document.getElementById("suggestionTwo").innerText;
+          // console.log(promptValue);
+          data.prompt = promptValue;
+          console.log(data);
+          const promptDiv = document.getElementById("writePrompt");
+          const li = document.createElement("li");
+          li.innerHTML = promptValue;
+          promptDiv.appendChild(li);
+
+          sendRequest()
+        }
+  //console.log(data);
+  myElementTwo.after(btn);
+}
+
+function getSuggestionsThird(myElementThree){
+  console.log(myElementThree);
+  let btn = document.createElement("button");
+  btn.innerHTML = "Select";
+
+      btn.onclick  = function() {
+        let promptValue = document.getElementById("suggestionThree").innerText;
+        // console.log(promptValue);
+        data.prompt = promptValue;
+        console.log(data);
+          const promptDiv = document.getElementById("writePrompt");
+          const li = document.createElement("li");
+          li.innerHTML = promptValue;
+          promptDiv.appendChild(li);
+
+          sendRequest()
+        }
+  //console.log(data);
+  myElementThree.after(btn);
+}
+
+getSuggestionsFirst(myElementOne);
+getSuggestionsSecond(myElementTwo);
+getSuggestionsThird(myElementThree);
+
+
+
+//////////////////CLEAR////////////////
+
+  //Clear prompt 
+  function clearRequest() {
+    document.getElementById("form").reset(); 
+    window.location.reload();
+  }
+
